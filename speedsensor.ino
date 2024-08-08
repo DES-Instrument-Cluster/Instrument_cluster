8d783716ff10e58b699689729b3954c6caf0535e
Clearsu <clearwater1219@gmail.com>
2024-08-08 19:16:38 +0200

    refactor: remove filter logic

--- speedsensor.ino
+++ speedsensor.ino
@@ -8,53 +8,37 @@ struct can_frame canMsg;
 MCP2515 mcp2515(10);
 
 const float distancePerPulse = 1.0367;
-const int movingAverageSize = 10;
-
-float speedBuffer[movingAverageSize];
-int bufferIndex = 0;
-float speedSum = 0.0;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(speedSensorPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(speedSensorPin), countPulses, RISING);
-  SPI.begin();
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
-  mcp2515.setNormalMode();
-
-  for (int i = 0; i < movingAverageSize; i++) {a
-    speedBuffer[i] = 0.0;
-  }
+    Serial.begin(9600);
+    pinMode(speedSensorPin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(speedSensorPin), countPulses, RISING);
+    SPI.begin();
+    mcp2515.reset();
+    mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
+    mcp2515.setNormalMode();
 }
 
 void loop() {
-  static unsigned long lastSendTime = 0;
-  unsigned long currentTime = millis();
-  if (currentTime - lastSendTime > 100) {
-    lastSendTime = currentTime;
+    static unsigned long lastSendTime = 0;
+    unsigned long currentTime = millis();
+    if (currentTime - lastSendTime > 100) {
+        lastSendTime = currentTime;
 
-    float speedCmPerS = speedPulses * distancePerPulse * 10;
+        float speedCmPerS = speedPulses * distancePerPulse * 10;
 
-    speedSum -= speedBuffer[bufferIndex];
-    speedBuffer[bufferIndex] = speedCmPerS;
-    speedSum += speedCmPerS;
-    bufferIndex = (bufferIndex + 1) % movingAverageSize;
+        canMsg.can_id  = 0x100;
+        canMsg.can_dlc = sizeof(speedCmPerS);
+        memcpy(canMsg.data, &speedCmPerS, sizeof(speedCmPerS));
 
-    float averageSpeedCmPerS = speedSum / movingAverageSize;
+        mcp2515.sendMessage(&canMsg);
+        Serial.print("Speed (cm/s): ");
+        Serial.println(speedCmPerS, 2);
 
-    canMsg.can_id  = 0x100;
-    canMsg.can_dlc = sizeof(averageSpeedCmPerS);
-    memcpy(canMsg.data, &averageSpeedCmPerS, sizeof(averageSpeedCmPerS));
-
-    mcp2515.sendMessage(&canMsg);
-    Serial.print("Speed (cm/s): ");
-    Serial.println(averageSpeedCmPerS, 2);
-
-    speedPulses = 0;
-  }
+        speedPulses = 0;
+    }
 }
 
 void countPulses() {
-  speedPulses++;
+    speedPulses++;
 }
